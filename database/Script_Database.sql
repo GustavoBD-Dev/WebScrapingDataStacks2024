b3b8836d787fa52a1b5be132c1c36f8124b48b1b
Gustavo Blas Duran <gustavoblasduran@gmail.com>
2024-06-17 23:14:40 -0600

    settings of table

--- database/Script_Database.sql
+++ database/Script_Database.sql
@@ -24,3 +24,21 @@ create table enterprise_tool(
 	id_tool numeric,
 	id_enterprise numeric
 );
+
+
+-- set primary keys and secuence autoincrement to tables
+
+create sequence enterprise_id_sequence owned by enterprise.id_enterprise;
+
+alter table enterprise alter column id_enterprise set default nextval('enterprise_id_sequence'); 
+
+update enterprise set id_enterprise = nextval('enterprise_id_sequence'); 
+
+-- command describe table
+SELECT * FROM information_schema.columns
+WHERE table_schema = 'public'
+   AND table_name   = 'enterprise';
+  
+-- see sequence
+SELECT * FROM information_schema.sequences;
+  
